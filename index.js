cfde50c3f939f88127c1507f6383cbb9bd2a8d4c
Tri-Fred <102637124+Tri-Fred@users.noreply.github.com>
2022-11-11 20:44:44 +1300

    Final commit.
    Failure. 5 months to make a game and the best i could do was a start button that didn't even work, that would've lead to a blank white screen

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
-
+var canvas, canvasContext;
 window.onload = function() {
+    var canvas, canvasContext;
+    
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
    
-    loop = setInterval(() => {
-       update();
-       render();
-    },1000/60);
+
 
 
    document.addEventListener('keydown', keyPressed);
@@ -18,10 +17,22 @@ window.onload = function() {
        }
 
    
-
+        setInterval(mainloop, 1000 / 50);
    }
 /*var name= prompt ("Enter Username");
 alert("Welcome " +name);*/
+
+var playerXpos = 30;
+var playerYpos = 0;
+const PLAYER_HEIGHT = 100;
+const PLAYER_WIDTH = 5;
+var playerYspeed = 7;
+
+var AIXpos = 750;
+var AIYpos = 0;
+const AI_HEIGHT = 100;
+const AI_WIDTH = 5;
+var AIYspeed = 7;
 
 var resetting = true;
 var score = 0;
@@ -48,119 +59,30 @@ var downKeyPressed = false;
 //toggle ball start position
 var startingPos = true;
 
-//player image
-var player = new Image
-   player.src = 'images/pencil.png';
+function mainloop(){
+    colorRect(0, 0, canvas.width, canvas.height, 'black');
+    colorRect(ballXpos, ballYpos, SIZE, SIZE, 'white');
+    colorRect(playerXpos, playerYpos, PLAYER_WIDTH, PLAYER_HEIGHT, 'white');
+    colorRect(AIXpos, AIYpos, AI_WIDTH, AI_HEIGHT, 'white');
 
-//ball image
-var ball = new Image ();
-   ball.src = 'images/ball.png';
+    colorText(50, 20, '20px arial', 'Score: ' + score, 'white');
+    colorText(canvas.width - 150, 20, '20px arial', 'AIScore:' + AIscore, 'white');
 
-
-
-
-function drawRect(x,y,width,height,color) {
-   canvasContext.fillstyle = color;
-   canvasContext.fillRect(x,y,width,height);
-   canvasContext.fill();
+    ballMove();
+    paddleMove();
+    if (resetting) {
+        reset();
+        resetting = false;
+    }
+    AImove();
+    drawlines();
 }
-//draw ball into canvas
-function drawCircle(x,y,size,color){
-   canvasContext.beginpath();
-   canvasContext.fillstyle= color;
-   canvasContext.arc(x,y,size,0,math.PI*2);
-   canvasContext.fill();
-}
-// draw dotted lines in middle of screen. Place for ball to reset to
-function drawNet() {
-   canvasContext.strokeStyle = "fff";
-   canvasContext.lineWidth = 5;
-   canvasContext.setLineDash ([20,10]);
-   canvasContext.beginPath();
-   canvasContext.moveTo(canvas.width/2, 0);
-   canvasContext.lineTo(canvas.width/2 ,canvas.height);
-   canvasContext.stroke();
-}
-
-
-   paddle1 = {
-       width:20,
-       height:140,               
-       x:10,
-       y:(canvas.height/2) -70,
-       color:"#fff",
-   }
-
-   paddle2 = {
-       width:20,
-       height:140,
-       x:(canvas.width)-30,
-       y:(canvas.height/2) -70,
-       color:"#fff",
-   }
-   ball = {
-       size:14,
-       x:(canvas.width/2),
-       y:(canvas.height/2),
-       xv:8,
-       yv:8,
-       color:"#fff",
-   }
-
-
-function update() {
-   
-}
-
-function start () {
-   update();
-   render();
-   init();
-   loop = setInterval(update,fps);
-}
-
-//start game code
-function startGame() {
-   let startDiv = document.getElementById("start");
-   let gameCanvas = document.getElementById("canvas");
-   let  gameOver = document.getElementById("game-over");
-   startDiv.style.display = "none";
-   gameCanvas.style.display = "block";
-   gameOver.style.display = "none";
-   start();
-}
-//end game code
-function gameOver() {
-   let startDiv = document.getElementById("start");
-   let gameCanvas = document.getElementById("canvas");
-   let  gameOver = document.getElementById("game-over");
-   startDiv.style.display = "none";
-   gameCanvas.style.display = "none";
-   gameOver.style.display = "block";
-
-   clearInterval(loop);
- }
-//final draw function
-function render() {
-   drawRect(0,0,canvas.width,canvas.height,"#000");
-   drawNet();
-   drawRect(paddle1.x,paddle1.y,paddle1.width,paddle1.height,paddle1.color);
-   drawRect(paddle2.x,paddle2.y,paddle2.width,paddle2.height,paddle2.color);
-   drawCircle(ball.x,ball.y,ball.size,ball.color);
-
-   ballMove();
-   playerMove();
-   enemyAImove();
-   drawlines();
-   
-   }
-
-//lines for bouncing code
 function drawlines(){
-   middlelines.forEach(function(line,i,array){
-       colorRect(line.lineXpos,line.lineYpos,line.WIDTH,line.HEIGHT,'white');
-   });
+    middlelines.forEach(function(line,i,array){
+        colorRext(line,lineXpos, line,lineYpos, line.WIDTH, line.HEIGHT,'white');
+    });
 }
+
 //line for bounce code
 var linecount = 0;
 
@@ -181,23 +103,13 @@ function makelines() {
    }
    middlelines.push(line);
 }
+
+
 //background colour
 
 function colorRect(x,y,w,h,c){
    canvasContext.fillStyle = c;
    canvasContext.fillRect(x,y,w,h);
-}
-
-//player draw function
-   function drawImg(player,PLAYER_X_POS,playerYpos,PLAYER_WIDTH,PLAYER_HEIGHT){
-canvasContext.drawImage (player,PLAYER_X_POS,playerYpos,PLAYER_WIDTH,PLAYER_HEIGHT);
-}
-
-
-//ball draw function
-   function drawImg(ball,ballXpos,ballYpos,BALL_SIZE,BALL_SIZE){
-canvasContext.drawImage (ball,ballXpos,ballYpos,BALL_SIZE,BALL_SIZE);
-
 }
 
 
@@ -207,22 +119,21 @@ canvasContext.drawImage (ball,ballXpos,ballYpos,BALL_SIZE,BALL_SIZE);
        ballYpos = ballYpos + ballYspeed;
 
        //ball bouncing off player goalie
-       if (ballXpos < PLAYER_X_POS + PLAYER_WIDTH && 
+       if (ballXpos < playerXpos + PLAYER_WIDTH && 
            ballYpos + BALL_SIZE > playerYpos &&
            ballYpos < playerYpos + PLAYER_HEIGHT) {
            ballXspeed *= -1;
        }
        //ball bouncing off enemy goalie
-       if (ballXpos + BALL_SIZE > enemyAIXpos &&
-           ballYpos + BALL_SIZE > enemyAIYpos &&
-           ballYpos < enemyAIYpos + ENEMYAI_HEIGHT) {
+       if (ballXpos + BALL_SIZE > AIXpos &&
+           ballYpos + BALL_SIZE > AIYpos &&
+           ballYpos < AIYpos + AI_HEIGHT) {
            ballXspeed *= -1;
        }  
        
        //ball bouncing off right edge
        if(ballXpos > canvas.width - BALL_SIZE){
            ballXspeed *= -1;
-           gameOver = true;
            startingPos = true;
            score++;
        }
@@ -230,7 +141,6 @@ canvasContext.drawImage (ball,ballXpos,ballYpos,BALL_SIZE,BALL_SIZE);
        //ball bouncing off left edge
        if(ballXpos < 0 ){
            ballXspeed *= -1;
-           gameOver = true;
            startingPos = true;
            AIscore++;
        }
@@ -278,13 +188,13 @@ function playerMove() {
 
    //AI following ball
    function enemyAImove() {
-       if(ballYpos < enemyAIYpos + ENEMYAI_HEIGHT / 3) {
-       enemyAIYpos -= enemyAIYspeed;
+       if(ballYpos < AIYpos + AI_HEIGHT / 3) {
+       AIYpos -= AIYspeed;
 
        }
 
-       if(ballYpos > enemyAIYpos + ENEMYAI_HEIGHT / 2) {
-       enemyAIYpos += enemyAIYspeed;                    
+       if(ballYpos > AIYpos + AI_HEIGHT / 2) {
+       AIYpos += AIYspeed;                    
        }
    }   
   
@@ -294,13 +204,13 @@ function playerMove() {
        ballXpos = canvas.width / 2 - BALL_SIZE / 2;
        ballYpos = canvas.height / 2 - BALL_SIZE / 2;
        playerYpos = ballYpos - PLAYER_HEIGHT / 2;
-       enemyAIYpos = ballYpos - ENEMYAI_HEIGHT / 2;
+       AIYpos = ballYpos - AI_HEIGHT / 2;
        
    }
    //scoreboard text
    function colorText(x, y, f, msg, c) {
        canvasContext.fillText(msg, x, y);
-       canvasContext.fillStyle = c
+       canvasContext.fillStyle = c;
        canvasContext.font = f;
        
    }
